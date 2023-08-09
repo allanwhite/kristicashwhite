f9fde4c8c20df62fc725c19382c3c14769645272
Allan White <allan.white@contentful.com>
2023-08-09 13:41:40 -0700

    WIP commit
    styling, favicons, font work

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,18 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			'sans': ['"Public Sans Variable"', 'sans-serif'],
-			'serif': ['"Playfair Display Variable"', 'serif'],
-		},
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        fontFamily: {
+            'sans': ['Clarika', 'ui-sans-serif', 'system-ui'],
+            'serif': ['Sentinel', 'ui-serif', 'Georgia'],
+            'mono': ['ui-monospace', 'SFMono-Regular'],
+            'display': ['Clarika'],
+            'body': ['Sentinel'],
+        },
+        extend: {},
+    },
+    plugins: [],
 };
+// 'sans': ['"Public Sans Variable"', 'sans-serif'],
+// 'serif': ['"Playfair Display Variable"', 'serif'],
