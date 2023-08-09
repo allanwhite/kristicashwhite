121ae433aabd28bb3aa6053ba7a4c5fb7e8040c3
Allan White <allan.white@contentful.com>
2023-08-08 20:25:14 -0700

    base landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'sans': ['"Public Sans Variable"', 'sans-serif'],
+			'serif': ['"Playfair Display Variable"', 'serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
