1d5a1dd54df8affbcb8da8ae95e918f8a277e751
Allan White <allan.white@contentful.com>
2023-08-10 13:27:10 -0700

    Cleanup, removing font includes
    I prefer to install fonts manually. More control.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-// Supports weights 400-900
-import '@fontsource-variable/playfair-display';
-// Supports weights 100-900
-import '@fontsource-variable/public-sans';
+
 
 // https://astro.build/config
 export default defineConfig({
